2d128295e3fdf1382c3eeb50f5a89b2eae9b79c7
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2024-05-20 21:58:40 +0530

    [003]: Added bg and color boxes on add edit note screen

--- app/src/main/java/com/example/composenotesapp/ui/add_edit_note/AddEditNoteScreen.kt
+++ app/src/main/java/com/example/composenotesapp/ui/add_edit_note/AddEditNoteScreen.kt
@@ -1,15 +1,55 @@
 package com.example.composenotesapp.ui.add_edit_note
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.dp
+import com.example.composenotesapp.ui.models.NotesUIModel
 
 @Composable
 fun AddEditNoteScreen() {
-    Box (
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Add Edit Notes Screen")
+    Scaffold { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(NotesUIModel.colors.random())
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                NotesUIModel.colors.forEach { color ->
+                    Box(
+                        modifier = Modifier
+                            .size(50.dp)
+                            .shadow(16.dp, CircleShape)
+                            .background(color)
+                            .clip(CircleShape)
+
+
+                    )
+                }
+            }
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/composenotesapp/ui/models/NotesUIModel.kt
@@ -0,0 +1,15 @@
+package com.example.composenotesapp.ui.models
+
+import com.example.composenotesapp.ui.theme.BabyBlue
+import com.example.composenotesapp.ui.theme.LightGreen
+import com.example.composenotesapp.ui.theme.RedOrange
+import com.example.composenotesapp.ui.theme.RedPink
+import com.example.composenotesapp.ui.theme.Violet
+
+class NotesUIModel {
+
+    companion object {
+        val colors = listOf(RedOrange, RedPink, BabyBlue, Violet, LightGreen)
+    }
+
+}
